7c3bfbc08951d51b8d28ee0b2f3f6044a503f6b3
Zach Stone <zachinjapan888s@gmail.com>
2021-10-26 16:49:38 -0400

    Starting Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Introduction from "./Components/Introduction";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my test App</h1>
+      <Introduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Introduction.js
@@ -0,0 +1,5 @@
+function Introduction(props) {
+  return <h3>My name is {props.name}</h3>;
+}
+
+export default Introduction;
